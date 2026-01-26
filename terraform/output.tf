df448af4999bf5808247a18f94523006df29861b
faizanmalik2070 <faizanmalik2070@gmail.com>
2026-01-26 21:37:59 +0500

    Create output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,15 @@
+
+
+output "lambda_name" {
+  value = aws_lambda_function.this.function_name
+}
+
+output "lambda_arn" {
+  value = aws_lambda_function.this.arn
+}
+
+/*
+output "lambda_public_url" {
+  value = aws_lambda_function_url.public.function_url
+}
+*/
